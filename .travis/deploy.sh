9c411e730107ba365a640423ef6bbba4c748dbea
Kirill Merkushev <lanwen@yandex.ru>
2017-06-13 18:46:10 +0300

    misc

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -4,7 +4,7 @@ then
     echo "on a tag -> set pom.xml <version> to $TRAVIS_TAG"
     mvn --settings .travis/settings.xml org.codehaus.mojo:versions-maven-plugin:2.1:set -DnewVersion=$TRAVIS_TAG 1>/dev/null 2>/dev/null
 else
-    echo "not on a tag -> keep snapshot version in pom.xml"
+    echo "not on a tag -> keep SNAPSHOT version in pom.xml"
 fi
 
 mvn clean deploy --settings .travis/settings.xml -DskipTests=true -B -U
