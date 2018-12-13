0424e52b03017a984cbd51bbcf10ad1adc345720
vincent <vincent@stage.tech>
2018-12-13 16:17:56 +0100

    + Add New Feature:   - ResponseTemplateTransformerWireMockConfigFactory + Test

--- src/main/java/ru/lanwen/wiremock/config/WiremockConfigFactory.java
+++ src/main/java/ru/lanwen/wiremock/config/WiremockConfigFactory.java
@@ -2,6 +2,7 @@ package ru.lanwen.wiremock.config;
 
 import com.github.tomakehurst.wiremock.common.Slf4jNotifier;
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+import com.github.tomakehurst.wiremock.extension.responsetemplating.ResponseTemplateTransformer;
 import ru.lanwen.wiremock.ext.WiremockResolver;
 
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
@@ -29,6 +30,20 @@ public interface WiremockConfigFactory {
         @Override
         public WireMockConfiguration create() {
             return options().dynamicPort().notifier(new Slf4jNotifier(true));
+        }
+    }
+
+    /**
+     * By default creates config with dynamic port only, notifier and Templating Response enabled.
+     */
+    class ResponseTemplateTransformerWireMockConfigFactory extends DefaultWiremockConfigFactory {
+
+        @Override
+        public WireMockConfiguration create() {
+            return super.create()
+                    // enable Templating Response!
+                    // @see : http://wiremock.org/docs/response-templating/
+                    .extensions(new ResponseTemplateTransformer(true));
         }
     }
 }
